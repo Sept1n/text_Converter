da4b01846b84d81a82511a020fc1a825ab51e79e
Sept1n <72445760+Sept1n@users.noreply.github.com>
2020-11-30 19:29:00 +0100

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Projekt
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 15);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(420, 200);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(278, 103);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Wyczyść";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(278, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Dalej";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 42);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(236, 162);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Tekst do wysłania";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(15, 225);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(420, 200);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(278, 103);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 33);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Wyczyść";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(278, 53);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 33);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Dalej";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(13, 42);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(236, 162);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tekst binarny";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.button5);
+            this.groupBox3.Controls.Add(this.textBox3);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(15, 435);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(420, 200);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(278, 99);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(129, 33);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Wyczyść wszystko";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(278, 48);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 33);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Wyczyść";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(13, 38);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(236, 162);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tekst odebrany";
+            // 
+            // Projekt
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 639);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Projekt";
+            this.Text = "Projekt";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        public System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        public System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Projekt : Form
+    {
+        public Projekt()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e) //czyszczenie pola
+        {
+            textBox1.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e) //czyszczenie pola
+        {
+            textBox2.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e) //czyszczenie pola
+        {
+            textBox3.Clear();
+        }
+
+        private void button6_Click(object sender, EventArgs e) //czyszczenie pól
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        public string Ascii(string asc)
+        {
+            char x = ' ';
+            byte[] byte1 = Encoding.ASCII.GetBytes(textBox1.Text); //pobranie wprowadzanego tekstu do tablicy 
+            string[] podzielone = asc.Split(x); //dzielenie
+            int a = podzielone.Length;
+
+            foreach (string b in podzielone) //usuwanie 
+            {
+                b.Remove(0);
+                b.Remove(8);
+                b.Remove(9);
+            }
+
+            string lecimy_dalej = string.Join("", podzielone);
+
+            int słowa = lecimy_dalej.Length;
+            int elementy = (słowa - 1) / 7;
+            List<Byte> byte2 = new List<Byte>();
+            int d = 6;
+            for (int c = 0; c < lecimy_dalej.Length; c = c + d) //od 0 co 0 + 6
+                byte2.Add(Convert.ToByte(lecimy_dalej.Substring(c, d), 2));
+            byte[] byte3 = new byte[byte2.Count];
+            byte2.CopyTo(byte3);
+            return Encoding.ASCII.GetString(byte3);
+        }
+        public string Bin(string bin) //dodawanie bitów startu i stopu
+        {
+            StringBuilder bla = new StringBuilder();
+            foreach (char p in bin.ToString())
+            {
+                bla.Append(Convert.ToString(p, 2));
+
+                bla.Append(11);
+                bla.Append(" ");
+                bla.Append(0);
+
+            }
+            bla.Insert(0, 0);
+            string n;
+            n = bla.ToString();
+            int x = n.Length;
+            return n.Remove(x - 2);
+        }
+
+        private static string Tekst(String ofc)
+        {
+            int x = 8;
+            List<Byte> byte1 = new List<Byte>();
+            for (int a = 0; a < ofc.Length; a = a + x) //Od 0 co 8
+                byte1.Add(Convert.ToByte(ofc.Substring(a, x), 2));
+            byte[] bytes2 = new byte[byte1.Count];
+            byte1.CopyTo(bytes2);
+            return Encoding.UTF8.GetString(bytes2); //zamiana na znaki
+        }
+
+        public string pamietaj()
+        {
+            byte[] byte1 = Encoding.ASCII.GetBytes(textBox1.Text);
+            string zapamiętane = Encoding.ASCII.GetString(byte1);
+            return zapamiętane;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string wysyłane = textBox1.Text;//pobieranie
+            char y = ' ';
+            string[] sklejanie = wysyłane.Split(y);//dzielenie co 1
+
+            int długość = sklejanie.Length; //cenzura
+            string[] brzydkie = { "kurwa", "pizda", "szmata", "jebie", "chuj", "huj", "pierdol", "Kurwa", "Pizda",
+                "Szmata", "Jebie", "Chuj", "Huj", "Pierdol", };
+            int length = brzydkie.Length;
+            for (int i = 0; i <= długość - 1; i++)
+            {
+                for (int j = 0; j <= length - 1; j++)
+                {
+                    if (sklejanie[i].Contains(brzydkie[j])) //porównywanie (nie widzi enterów (jeśli słowo wulgarne nie jest oddzielone 
+                        //spacją to traktowane jest jako jedno długie słowo razem z nieoddzielonym wyrazem i oba są zamieniane na "*"))
+                    {
+                        string słowo = sklejanie[i];
+                        string wulgaryzm = brzydkie[j];
+                        string sprawdzanie = sklejanie[i];
+                        foreach (char u in słowo)
+                        {
+                            char w = '*';       //zastępowanie wulgaryzmów przez "*"
+                            sprawdzanie = sprawdzanie.Replace(u, w);
+                            sklejanie[i] = sklejanie[i].Replace(sklejanie[i], sprawdzanie);
+                        }
+                        sklejanie[i].Replace(sklejanie[i], sprawdzanie);
+                        string poprawianie = string.Join(",", sklejanie);
+                        textBox3.Text = sprawdzanie;
+                    }
+                    else
+                        textBox3.Text = textBox1.Text;
+
+                    string nowość = string.Join(" ", sklejanie);
+                    textBox1.Text = nowość;
+                    textBox3.Text = nowość;
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)//zamiana na binarne
+        {
+            textBox2.Text = Bin(textBox1.Text);
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+       /*public void sprawdzanie(string g)
+       {
+           char h = ' ';
+           string wysyłane = textBox1.Text;
+           string[] sklejanie = wysyłane.Split(h);
+
+           int długość = sklejanie.Length;
+           string[] brzydkie = { "kurwa", "pizda" };
+           int length = brzydkie.Length;
+           for (int i = 0; i <= długość - 1; i++)
+           {
+               for (int j = 0; j <= length - 1; j++)
+               {
+                   if (sklejanie[i].Contains(brzydkie[j]))
+                   {
+                       string słowo = sklejanie[i];
+                       string wulgaryzm = brzydkie[j];
+                       string sprawdzanie = sklejanie[i];
+                       foreach (char u in słowo)
+                       {
+                           char w = '*';
+                           sprawdzanie = sprawdzanie.Replace(u, w);
+                           sklejanie[i] = sklejanie[i].Replace(sklejanie[i], sprawdzanie);
+                       }
+                       sklejanie[i].Replace(sklejanie[i], sprawdzanie);
+                       string poprawianie = string.Join(",", sklejanie);
+                       textBox3.Text = sprawdzanie;
+                   }
+                   else
+                       textBox3.Text = textBox1.Text;
+
+                   string nowość = string.Join(" ", sklejanie);
+                   textBox1.Text = nowość;
+               }
+
+           }
+       }*/
+    }
+}
